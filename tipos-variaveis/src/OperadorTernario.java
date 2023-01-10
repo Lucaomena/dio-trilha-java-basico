eb921625068f2fa9badbaf784c00762633963898
Lucaomena <lucaomena32@gmail.com>
2023-01-09 22:47:20 -0300

    Operadores Ternarios

---
+++ tipos-variaveis/src/OperadorTernario.java
@@ -0,0 +1,15 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int a,b;
+
+        a = 5;
+        b = 6;
+
+        String resultado1 = a == b ? "verdadeiro" : "Falso";
+        int resultado2 = a == b ? 1: 0;
+         
+               System.out.println("Resultado: " + resultado1);
+               System.out.println("Resultado: " + resultado2);
+    }
+    
+}
