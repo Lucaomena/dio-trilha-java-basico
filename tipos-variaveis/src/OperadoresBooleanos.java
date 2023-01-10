8d2108c73296dfd9754db71030ac1d6fc56d1dca
Lucaomena <lucaomena32@gmail.com>
2023-01-09 22:37:44 -0300

    Operador Booleano

---
+++ tipos-variaveis/src/OperadoresBooleanos.java
@@ -0,0 +1,14 @@
+public class OperadoresBooleanos {
+
+    public static void main(String[] args) {
+        boolean variavel = true;
+
+        //inversao de valores da variavel
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+        
+    }
+    
+}
