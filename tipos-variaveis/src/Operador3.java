6c23cbd604998d4748973c83bff7891b2fe99de4
Lucaomena <lucaomena32@gmail.com>
2023-01-09 22:25:45 -0300

    Operadores 4 -Incrementando e decrementando

--- tipos-variaveis/src/Operador3.java
+++ tipos-variaveis/src/Operador3.java
@@ -3,6 +3,8 @@ public class Operador3 {
     public static void main(String[] args) {
         int numero = -5;
 
+        numero= - numero;
+
         System.out.println(-- numero);
     }
 }
---
+++ tipos-variaveis/src/Operadores4.java
@@ -0,0 +1,12 @@
+public class Operadores4 {
+
+    public static void main(String[] args) {
+        int numero = 5;
+        //x repeticao
+        numero++;
+        //numero = numero +1;
+
+        System.out.println(numero);
+    }
+    
+}
